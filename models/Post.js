35906743a26f6ae52609b486fdb72cb371054d3b
fortu038 <fortu038@umn.edu>
2022-10-28 00:43:42 -0500

    added models, utility function, and updated routes

---
+++ models/Post.js
@@ -0,0 +1,35 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection.js");
+
+class Post extends Model {}
+
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        text: {
+            type: DataTypes.TEXT,
+            allowNull: true,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: "user",
+                key: "id",
+            }, 
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "post"
+    }
+);
+
+module.exports = Post;
---
+++ models/User.js
@@ -0,0 +1,72 @@
+const { Model, DataTypes } = require("sequelize");
+const bcrypt = require("bcrypt");
+const sequelize = require("../config/connection.js");
+
+class User extends Model {
+    async checkPassword(loginPw) {
+        return await bcrypt.compare(loginPw, this.password);
+    }
+}
+
+User.init(
+  {
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [8],
+        }
+    },
+    // comment_id: {
+    //     type: DataTypes.INTEGER,
+    //     references: {
+    //         model: "comment",
+    //         key: "id",
+    //     },
+    // },
+    // post_id: {
+    //     type: DataTypes.INTEGER,
+    //     references: {
+    //         model: "post",
+    //         key: "id",
+    //     }, 
+    // }
+  },
+  
+  { 
+    hooks: {
+        beforeCreate: async (newUserData) => {
+            newUserData.password = await bcrypt.hash(newUserData.password, 10);
+            return newUserData;
+        },
+        beforeUpdate: async (updatedUserData) => {
+            if (updatedUserData.password) {
+            updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+            }
+            return updatedUserData;
+        },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user'
+  }
+);
+
+module.exports = User;
