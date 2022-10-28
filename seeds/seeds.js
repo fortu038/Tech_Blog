8626dc49a207bf826afbd2f56be330f136a61d33
fortu038 <fortu038@umn.edu>
2022-10-28 17:19:34 -0500

    got seeding files and function working

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,19 +1,22 @@
+const sequelize = require('../config/connection');
+const { User, Comment, Post } = require('../models');
+
 const postData = require('./postData.json');
 const commentData = require('./commentData.json');
 const userData = require('./userData.json');
 
 
 const seedDatabase = async () => {
-  await sequelize.sync({ force: true });
-  await User.bulkCreate(userData, {
-    individualHooks: true,
-    returning: true,
-  });
+    await sequelize.sync({ force: true });
+    await User.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
+    });
 
-  Comment.bulkCreate(commentData, {
-  });
+    await Post.bulkCreate(postData, {
+    });
 
-  Post.bulkCreate(postData, {
-  });
+    await Comment.bulkCreate(commentData, {
+    });
 };
 seedDatabase();
