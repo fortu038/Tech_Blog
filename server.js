d2184b2945f90cd1b0eee926044a9cc208143b66
fortu038 <fortu038@umn.edu>
2022-11-01 12:26:12 -0500

    modified some files

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const hbs = exphbs.create({});
 const app = express();
 const PORT = process.env.PORT || 3001;
 const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
   secret: 'Super secret secret',
